1d5f067351d81bc6e115c762db87dd50d0175a65
kudoreleasebot <release@kudo.dev>
2019-08-02 17:10:49 +0200

    Brew formula update for kudo version v0.5.0

--- Formula/kudo-cli.rb
+++ Formula/kudo-cli.rb
@@ -2,16 +2,9 @@
 class KudoCli < Formula
   desc "Interact with KUDO via the kubectl plugin"
   homepage "https://kudo.dev"
-  version "0.4.0"
-  bottle :unneeded
-
-  if OS.mac?
-    url "https://github.com/kudobuilder/kudo/releases/download/v0.4.0/kudo_0.4.0_darwin_x86_64.tar.gz"
-    sha256 "9acd925cedddd7661c929b28b30d50ca27b6e9bbbc3c80fe6b8dd72caefdd005"
-  elsif OS.linux?
-    url "https://github.com/kudobuilder/kudo/releases/download/v0.4.0/kudo_0.4.0_linux_x86_64.tar.gz"
-    sha256 "c638b3d047d8d93d524bd641731cab3c11f21f4d951907b5635f72301f871e88"
-  end
+  url "https://github.com/kudobuilder/kudo/releases/download/v0.5.0/kudo_0.5.0_darwin_x86_64.tar.gz"
+  version "0.5.0"
+  sha256 "5a28fd7fa8a7c4fbe04eea74182c6040e00417918c6b211fd48fe0bda54a1e12"
   
   depends_on "kubernetes-cli"
 
