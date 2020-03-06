fff59e4c3d420860c50b256f2bdb1754cffade06
Ken Sipe <kensipe@gmail.com>
2020-03-06 15:06:40 -0600

    Brew formula update for kudo version v0.11.0-rc1
    Signed-off-by: Ken Sipe <kensipe@gmail.com>

--- Formula/kudo-cli.rb
+++ Formula/kudo-cli.rb
@@ -2,26 +2,26 @@
 class KudoCli < Formula
   desc "Interact with KUDO via the kubectl plugin"
   homepage "https://kudo.dev"
-  version "0.10.1"
+  version "0.11.0-rc1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/kudobuilder/kudo/releases/download/v0.10.1/kudo_0.10.1_darwin_x86_64.tar.gz"
-    sha256 "be194f42f0ef0d31e1481dca0051fd65103e7dbc45af2b745912a0fb2e4f780c"
+    url "https://github.com/kudobuilder/kudo/releases/download/v0.11.0-rc1/kudo_0.11.0-rc1_darwin_x86_64.tar.gz"
+    sha256 "be75cad6bfe645a795dea0644a57113b4f204f4e725824766b97f4b3f7311e37"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/kudobuilder/kudo/releases/download/v0.10.1/kudo_0.10.1_linux_x86_64.tar.gz"
-      sha256 "8640e1a887f23fa61a3f07e17bdd6cddf4b9e7c8ce8a64cf1bb12c0efa3eb490"
+      url "https://github.com/kudobuilder/kudo/releases/download/v0.11.0-rc1/kudo_0.11.0-rc1_linux_x86_64.tar.gz"
+      sha256 "1fd4c8f51e12c7db7e26468be1ff926a9b7c591dda6ec86081b01593dde4dce7"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
       else
-        url "https://github.com/kudobuilder/kudo/releases/download/v0.10.1/kudo_0.10.1_linux_armv6.tar.gz"
-        sha256 "273d52e5f15ba9223ace58b4e3d1428a4ee56222a4d61ea27a5c77958082154a"
+        url "https://github.com/kudobuilder/kudo/releases/download/v0.11.0-rc1/kudo_0.11.0-rc1_linux_armv6.tar.gz"
+        sha256 "d897f950021ee61e4629db5342e22d9c6669e1e13208d49a99da266da6ce54b2"
       end
     end
   end
-  
+
   depends_on "kubernetes-cli"
 
   def install
