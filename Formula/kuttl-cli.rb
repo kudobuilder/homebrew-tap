929e21a45b051d079026c156282c281b5b07785e
kudoreleasebot <release@kudo.dev>
2020-08-10 13:24:28 -0500

    Brew formula update for kuttl version v0.6.0

--- Formula/kuttl-cli.rb
+++ Formula/kuttl-cli.rb
@@ -2,22 +2,22 @@
 class KuttlCli < Formula
   desc "Interact with KUTTL via the kubectl plugin"
   homepage "https://kudo.dev"
-  version "0.5.1"
+  version "0.6.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/kudobuilder/kuttl/releases/download/v0.5.1/kuttl_0.5.1_darwin_x86_64.tar.gz"
-    sha256 "93806abdab27a2df2784c92b5fe818e9e30bf771944a1cd33afc9dccf11897d4"
+    url "https://github.com/kudobuilder/kuttl/releases/download/v0.6.0/kuttl_0.6.0_darwin_x86_64.tar.gz"
+    sha256 "50069e9ec397576886950dbba4f011443c2da4bcfba155a08de01cc741b51a0e"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/kudobuilder/kuttl/releases/download/v0.5.1/kuttl_0.5.1_linux_x86_64.tar.gz"
-      sha256 "27070b0b99f54cbacd9afd8845b07b3a678e2dde2acb1304a3c003656e987939"
+      url "https://github.com/kudobuilder/kuttl/releases/download/v0.6.0/kuttl_0.6.0_linux_x86_64.tar.gz"
+      sha256 "89e05ec1296b2166977855f56b242e07c49195527aba6afa0c3af223b38edb6f"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
       else
-        url "https://github.com/kudobuilder/kuttl/releases/download/v0.5.1/kuttl_0.5.1_linux_armv6.tar.gz"
-        sha256 "7372ec00009013e20f7a7060e865788d4da56b3012d85b82337c20ddbb4af0cc"
+        url "https://github.com/kudobuilder/kuttl/releases/download/v0.6.0/kuttl_0.6.0_linux_armv6.tar.gz"
+        sha256 "e9574c1f95556561a8b011030c806d2424ee51b97273aba85e9038afd4ea2b18"
       end
     end
   end
